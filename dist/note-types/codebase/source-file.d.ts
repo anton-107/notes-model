39c45e9d4b9ed39372b5bf39a5d76513b5431834
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2023-04-05 23:26:38 +0200

    interfaces for source file notes

---
+++ dist/note-types/codebase/source-file.d.ts
@@ -0,0 +1,19 @@
+import { JSONEncodedString } from "../../http-form";
+export interface SourceFileContributor {
+    name: string;
+    numberOfChanges: number;
+    firstChangeTimestamp: number;
+    lastChangeTimestamp: number;
+}
+export type SourceFileHandlerExtensionProperties = {
+    numberOfLines: string;
+    numberOfChanges: string;
+    numberOfContributors: string;
+    contributors?: SourceFileContributor[];
+};
+export interface SourceFileNoteCreateUpdateRequest {
+    "number-of-lines": string;
+    "number-of-changes": string;
+    "number-of-contributors": string;
+    "contributors": JSONEncodedString;
+}
---
+++ src/note-types/codebase/source-file.ts
@@ -0,0 +1,23 @@
+import { JSONEncodedString } from "../../http-form";
+import { Note } from "../../note-model";
+
+export interface SourceFileContributor {
+  name: string;
+  numberOfChanges: number;
+  firstChangeTimestamp: number;
+  lastChangeTimestamp: number;
+}
+
+export type SourceFileHandlerExtensionProperties = {
+  numberOfLines: string;
+  numberOfChanges: string;
+  numberOfContributors: string;
+  contributors?: SourceFileContributor[];
+};
+
+export interface SourceFileNoteCreateUpdateRequest {
+  "number-of-lines": string;
+  "number-of-changes": string;
+  "number-of-contributors": string;
+  "contributors": JSONEncodedString;
+}
